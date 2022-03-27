fd28e18bcf89e15a4ace2c3e5a1764ee91ecc8ad
Stephen Jimenez <bacchus_16@hotmail.com>
2022-03-27 16:41:04 -0400

    First commit missed some updated files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Navbar from './components/Navbar'
+import Location from './components/Location'
+import Data from './Data.js'
 
 function App() {
+  const info = Data.map(function(item) {
+    return (
+      <Location 
+        image={item.imageUrl}
+        location={item.location}
+        map={item.googleMapsUrl}
+        title={item.title}
+        start={item.startDate}
+        end={item.endDate}
+        description={item.description}
+      />
+    )
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      {info}
     </div>
   );
 }
